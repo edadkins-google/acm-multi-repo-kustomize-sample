2be6cdad2856aef2cb80eeb7f9d7b6b2953eec97
Ed Adkins <ejmadkins>
2021-09-20 20:13:21 +0100

    fix file path

--- script/render.sh
+++ script/render.sh
@@ -1,3 +1,3 @@
-kustomize build source/environments/dev --output=deploy/dev/manifest.yaml
-kustomize build source/environments/pre-prod --output=deploy/pre-prod/manifest.yaml
-kustomize build source/environments/prod --output=deploy/prod/manifest.yaml
+kustomize build ../source/environments/dev --output=../deploy/dev/manifest.yaml
+kustomize build ../source/environments/pre-prod --output=../deploy/pre-prod/manifest.yaml
+kustomize build ../source/environments/prod --output=../deploy/prod/manifest.yaml
