7a6678f7e50a6a7cd069bddbd7436ebd53dd67dc
Ed Adkins <ejmadkins>
2021-09-22 11:32:41 +0100

    add support for multi-repo

--- script/render.sh
+++ script/render.sh
@@ -1,10 +1,17 @@
-for dir in ../source/environments/*/     # list directories in the form "/tmp/dirname/"
+#!/bin/bash
+
+# Render kustomizations
+
+set -o errexit -o nounset -o pipefail
+
+for dir in ../source/environments/*/
 do
-    dir=${dir%*/}      # remove the trailing "/"
-    for cluster in ../source/environments/"${dir##*/}"/*/     # list directories in the form "/tmp/dirname/"
+    dir=${dir%*/}
+    for cluster in ../source/environments/"${dir##*/}"/*/
     do
        cluster=${cluster%*/}
        echo "rendering ${cluster##*/} in ${dir##*/}..."
+       mkdir -p ../deploy/"${dir##*/}"/"${cluster##*/}"
        kustomize build ${cluster} --output=../deploy/"${dir##*/}"/"${cluster##*/}"/manifest.yaml
     done
 done
